6ecce9417978911978811c43b29727023b5010fb
Shane McCarron <85966487+Corpypooh@users.noreply.github.com>
2022-04-04 11:18:31 -0400

    Various comments to the blocks we will be using for our program

--- SDR_CycloneIII/pwm.sv
+++ SDR_CycloneIII/pwm.sv
@@ -14,17 +14,17 @@ module pwm # (
     logic [WIDTH-1:0]                   compare_reg;
     
     always_ff @ (posedge clock) begin
-        if (clock_sreset) begin
+        if (clock_sreset) begin // reset, sets counter, data_reg, pwm_out to 0, but sets compare_reg to 1
             counter <= {WIDTH{1'b0}};
             data_reg <= {WIDTH{1'b0}};
             compare_reg <=  ONE[WIDTH-1:0] << (WIDTH-1);
             pwm_out <= 1'b0;
         end
         else begin
-            if (data_valid) begin
+            if (data_valid) begin // if data_valid -1, then set data_reg to
                 data_reg <= ONE[WIDTH-1:0] + data + {1'b0, {WIDTH-1{1'b1}}};
             end
-            counter <= counter + ONE[WIDTH-1:0];
+                counter <= counter + ONE[WIDTH-1:0]; // adds 1 to counter
             if (&counter) begin
                 compare_reg <= data_reg;
             end
